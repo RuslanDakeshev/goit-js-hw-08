27784dc5c5f3d0bd24f279f7e7c1d2cc4ffcf55e
RuslanDakeshev <103747764+RuslanDakeshev@users.noreply.github.com>
2022-09-22 22:25:45 +0000

    Deploying to gh-pages from @ RuslanDakeshev/goit-js-hw-08@b9089781ca56a2365548689f906230a688aa7e55 🚀

---
+++ 03-feedback.4fdfc878.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".feedback-form");console.log(e);const t=document.querySelector(".feedback-form textarea");console.log(t),e.addEventListener("submit",(function(e){e.preventDefault(),console.log(aaa)})),t.addEventListener("input",(function(e){const t=e.currentTarget.value;localStorage.setItem("feedback-msg",t)}));
+//# sourceMappingURL=03-feedback.4fdfc878.js.map
---
+++ 03-feedback.9184ea06.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".feedback-form");console.log(e);var t=document.querySelector(".feedback-form textarea");console.log(t),e.addEventListener("submit",(function(e){e.preventDefault(),console.log(aaa)})),t.addEventListener("input",(function(e){var t=e.currentTarget.value;localStorage.setItem("feedback-msg",t)}))}();
+//# sourceMappingURL=03-feedback.9184ea06.js.map
