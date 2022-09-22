b9089781ca56a2365548689f906230a688aa7e55
RuslanDakeshev <bil08@outlook.com>
2022-09-23 00:25:08 +0200

    03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,3 +1,19 @@
-
 const form = document.querySelector('.feedback-form');
 console.log(form);
+
+const textArea = document.querySelector('.feedback-form textarea');
+console.log(textArea);
+
+form.addEventListener('submit', onFormSubmit);
+textArea.addEventListener('input', onTextAreaInput);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  console.log(aaa);
+}
+
+function onTextAreaInput(event) {
+  const message = event.currentTarget.value;
+
+  localStorage.setItem('feedback-msg', message);
+}
