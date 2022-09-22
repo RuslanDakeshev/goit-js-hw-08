355b4432bbb9c2ede58cfe5be3eb42856bcda385
RuslanDakeshev <bil08@outlook.com>
2022-09-22 23:59:30 +0200

    gallery

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,6 +2,33 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
 
-console.log(12);
+const galleryEl = document.querySelector('.gallery');
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `<li class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+     
+      alt="${description}"
+    />
+  </a>
+</li>`;
+    })
+    .join('');
+}
+
+galleryEl.insertAdjacentHTML('beforeend', createGalleryMarkup(galleryItems));
+
+new SimpleLightbox('.gallery__link', {
+  captionsData: `alt`,
+  captionDelay: 250,
+});
