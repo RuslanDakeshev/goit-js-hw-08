447275e486eee87faa1cb5fdea6a3495aa1eb8ce
RuslanDakeshev <bil08@outlook.com>
2022-09-27 22:45:30 +0200

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,68 +4,83 @@ import throttle from 'lodash.throttle';
 
 const form = document.querySelector('.feedback-form');
 const textArea = document.querySelector('.feedback-form textarea');
-const inputEmail = document.querySelector('.feedback-form input');
+const input = document.querySelector('.feedback-form input');
 const formData = {};
 
 form.addEventListener('submit', onFormSubmit);
-textArea.addEventListener('input', throttle(onTextAreaInput, 500));
-inputEmail.addEventListener('input', throttle(onTextAreaInputEmail, 500));
-form.addEventListener('input', event => {
-  // console.log(event.target.name);
-  // console.log(event.target.value);
+// textArea.addEventListener('input', throttle(onFormDataInput, 500));
+form.addEventListener('input', throttle(onFormDataInput, 500));
+// form.addEventListener('input', event => {
+//   // console.log(event.target.name);
+//   // console.log(event.target.value);
 
-  formData[event.target.name] = event.target.value;
+//   formData[event.target.name] = event.target.value;
 
-  localStorage.setItem('feedback-form-state', JSON.stringify(formData));
-});
+//   localStorage.setItem('feedback-form-state', JSON.stringify(formData));
+// });
 
-inputEmail.addEventListener('input', event => {
-  // console.log(event.target.name);
-  // console.log(event.target.value);
-});
+// input.addEventListener('input', event => {
+//   // console.log(event.target.name);
+//   // console.log(event.target.value);
+// });
 
-const STORAGE_KEY = 'feedback-msg';
-const STORAGE_EMAIL = 'feedback-email';
+// const STORAGE_KEY = 'feedback-msg';
+// const STORAGE_EMAIL = 'feedback-email';
 
-populateTextarea();
+populateFormData();
 
 function onFormSubmit(event) {
   event.preventDefault();
+  if (!formData[input.name] || !formData[textArea.name]) {
+    alert('Все поля формы должны быть заполнены')
+  } else {
+    console.log(formData);
+  }
+  
   console.log('Отправка формы');
 
   event.currentTarget.reset();
 
-  const restoredSession = JSON.parse(
-    localStorage.getItem('feedback-form-state')
-  );
-  console.log(restoredSession);
+  // const restoredSession = JSON.parse(
+  //   localStorage.getItem('feedback-form-state')
+  // );
+  // console.log(restoredSession);
 
-  localStorage.removeItem(STORAGE_KEY);
-  localStorage.removeItem(STORAGE_EMAIL);
+  // localStorage.removeItem(STORAGE_KEY);
+  // localStorage.removeItem(STORAGE_EMAIL);
   localStorage.removeItem('feedback-form-state');
+  formData[input.name] = '';
+  formData[textArea.name] = '';
 }
 
-function onTextAreaInput(event) {
-  const message = event.target.value;
+  function onFormDataInput(event) {
+    formData[event.target.name] = event.target.value;
 
-  localStorage.setItem(STORAGE_KEY, message);
-}
-
-function onTextAreaInputEmail(event) {
-  const message = event.target.value;
-
-  localStorage.setItem(STORAGE_EMAIL, message);
-}
-
-function populateTextarea() {
-  const saveMessage = localStorage.getItem(STORAGE_KEY);
-  const saveEmail = localStorage.getItem(STORAGE_EMAIL);
-  if ((saveMessage, saveEmail)) {
-    console.log(saveMessage, saveEmail);
+    localStorage.setItem('feedback-form-state', JSON.stringify(formData));
   }
 
-  textArea.value = saveMessage;
-  inputEmail.value = saveEmail;
+  // function onTextAreaInputEmail(event) {
+  //   const message = event.target.value;
+
+  //   localStorage.setItem(STORAGE_EMAIL, message);
+  // }
+
+function populateFormData() {
+  const saveMessage = localStorage.getItem('feedback-form-state');
+  const parsedData = JSON.parse(saveMessage);
+  if (saveMessage) {
+    if (parsedData.email) {
+      input.value = parsedData.email;
+      formData[input.name] = parsedData.email;
+    }
+    if (parsedData.message) {
+      textArea.value = parsedData.message;
+      formData[textArea.name] = parsedData.message;
+    }
+  }
+
+  // textArea.value = saveMessage;
+  // input.value = saveEmail;
 }
 
 // import throttle from 'lodash.throttle';
